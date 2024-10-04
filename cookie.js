9e11bf1ccf1fef61029284476ddabf7ca24b6eb0
Ziadtoumi <9022337@student.zadkine.nl>
2024-10-04 09:26:33 +0200

    autoclickers

--- cookie.js
+++ cookie.js
@@ -2,7 +2,10 @@ class Clicker {
     constructor(start) {
         this.count = start;
         this.cookiesPerClick = 1;
+        this.autoClickers = 0;
+        this.autoClickRate = 0;
         this.displayCount();
+        this.startAutoClickers();
     }
 
     click() {
@@ -22,20 +25,41 @@ class Clicker {
             buttonElement.style.display = 'none';
             this.displayCount();
         } else {    
-                alert("Not enough cookies!");
+            alert("Not enough cookies!");
         }
+    }
+
+    purchaseAutoClicker(cost, rate, buttonElement) {
+        if (this.count >= cost) {
+            this.count -= cost;
+            this.autoClickRate += rate;
+            buttonElement.style.display = 'none';
+            this.displayCount();
+        } else {
+            alert("Not enough cookies!");
+        }
+    }
+
+    startAutoClickers() {
+        setInterval(() => {
+            if (this.autoClickRate > 0) {
+                this.count += this.autoClickRate;
+                this.displayCount();
+            }
+        }, 1000); // Adds cookies every second
     }
 }
 
 let clicker1 = new Clicker(0);
 
+// Cookie clicker
 const cookie = document.getElementById('cookie');
 cookie.addEventListener('click', () => {
     clicker1.click();
 });
 
-const doubleClickerButton = document.querySelector('.upgrade-item:nth-child(1)');
-doubleClickerButton = document.querySelector('.upgrade-item:nth-child(2)');
+// Upgrades
+const doubleClickerButton = document.querySelector('.upgrade-item:nth-child(2)');
 const tripleClickerButton = document.querySelector('.upgrade-item:nth-child(3)');
 const quadrupleClickerButton = document.querySelector('.upgrade-item:nth-child(4)');
 
@@ -49,4 +73,21 @@ tripleClickerButton.addEventListener('click', () => {
 
 quadrupleClickerButton.addEventListener('click', () => {
     clicker1.purchaseUpgrade(750, 4, quadrupleClickerButton);
+});
+
+// Auto-clickers
+const autoClickerButton = document.querySelector('.auto-clicker-item:nth-child(2)');
+const superClickerButton = document.querySelector('.auto-clicker-item:nth-child(3)');
+const megaClickerButton = document.querySelector('.auto-clicker-item:nth-child(4)');
+
+autoClickerButton.addEventListener('click', () => {
+    clicker1.purchaseAutoClicker(750, 5, autoClickerButton); // 1 cookie per second
+});
+
+superClickerButton.addEventListener('click', () => {
+    clicker1.purchaseAutoClicker(5000, 200, superClickerButton); // 5 cookies per second
+});
+
+megaClickerButton.addEventListener('click', () => {
+    clicker1.purchaseAutoClicker(10000, 1000, megaClickerButton); // 10 cookies per second
 });
